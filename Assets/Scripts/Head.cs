1269f869890eada84b4bed1d30efb9975e0e02ac
KDphan <kdphan1992@gmail.com>
2021-03-07 16:50:11 -0500

    got rid of ui elements and added weight bar ui and reused code from power game

--- Assets/Scripts/Head.cs
+++ Assets/Scripts/Head.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
@@ -6,6 +11,15 @@ namespace Assets.Scripts
     {
         public AudioSource _small_head;
         public AudioSource _big_head;
+        public int _currentWeight = 0;
+        public int _maxWeight = 100;
+
+        public WeightBar _weightBar;
+
+        private void Start()
+        {
+            _currentWeight = 0;
+        }
         
         private void OnCollisionEnter2D(Collision2D collision)
         {
@@ -17,6 +31,7 @@ namespace Assets.Scripts
 
             if (collision.gameObject.CompareTag("SmarterObject"))
             {
+                GainWeight(2);
                 _big_head.Play();
                 transform.localScale += new Vector3(Config.HEAD_GROWTH_SCALE, Config.HEAD_GROWTH_SCALE);                
             }
@@ -24,12 +39,27 @@ namespace Assets.Scripts
             {
                 var newScale = transform.localScale - new Vector3(Config.HEAD_GROWTH_SCALE, Config.HEAD_GROWTH_SCALE);
                 _small_head.Play();
+                LoseWeight(2);
 
                 if (newScale.x > 0 && newScale.y > 0)
                     transform.localScale = newScale;
             }
 
             Destroy(collision.gameObject);
+        }
+
+        public void GainWeight(int weightIncrease)
+        {
+            _currentWeight = Math.Min(100, _currentWeight + weightIncrease);
+
+            _weightBar.SetWeight(_currentWeight);
+        }
+
+        public void LoseWeight(int weightDecrease)
+        {
+            _currentWeight = Math.Max(0, _currentWeight - weightDecrease);
+
+            _weightBar.SetWeight(_currentWeight);
         }
     }
 }
