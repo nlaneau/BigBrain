2cdfa34c022f4ee2b6bdff4d0d495c7b674d6d0e
nlaneau <31058891+nlaneau@users.noreply.github.com>
2021-03-07 17:57:36 -0500

    Added GameTimer that restarts the scene every 30s

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    class GameTimer : MonoBehaviour
+    {
+        public int _startTime = 30;
+        private Text Text;
+
+        private void Start()
+        {
+            Text = GetComponent<Text>();
+        }
+
+        private float Timer = 0f;
+
+        private void Update()
+        {
+            Timer += Time.deltaTime;
+
+            var currentTime = _startTime - (int)Timer % 60;
+
+            Text.text = currentTime.ToString();
+
+            if (currentTime <= 0)
+            {
+                // Do gameover check
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+}
