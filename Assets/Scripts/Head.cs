88e62acee18d5b4862f3339338eee836664624ef
Anne Do <anne.do.designs@gmail.com>
2021-03-06 20:12:21 -0500

    Added head physics 2D material, adjusted colliders and hing joint

--- Assets/Scripts/Head.cs
+++ Assets/Scripts/Head.cs
@@ -6,6 +6,10 @@ namespace Assets.Scripts
     {
         private void OnCollisionEnter2D(Collision2D collision)
         {
+            if (collision.gameObject.CompareTag("Player")) {
+                return;
+            }
+
             if (!collision.gameObject.CompareTag("Environment"))
             {
                 transform.localScale += new Vector3(Config.HEAD_GROWTH_SCALE, Config.HEAD_GROWTH_SCALE);
