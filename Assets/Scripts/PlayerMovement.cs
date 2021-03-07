0a5b9b90495e82f569a745de85d42ddffd9cbe73
Anne Do <anne.do.designs@gmail.com>
2021-03-07 01:21:21 -0500

    Flip boy to face direction of movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,36 @@ namespace Assets.Scripts
     class PlayerMovement : MonoBehaviour
     {
         private Rigidbody2D _rigidbody2D;
-        
+        private GameObject _body;
+        private GameObject _head;
+
         private void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
+            _body = GameObject.Find("Body");
+            _head = GameObject.Find("Head");
+        }
+
+        private void RotateBoy()
+        {
+            _body.transform.Rotate(0, 180, 0, Space.Self);
+            _head.transform.Rotate(0, 180, 0, Space.Self);
         }
 
         private void Update()
         {
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && _body.transform.rotation.y != 0)
+            {
+                RotateBoy();
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow) && _body.transform.rotation.y != 180)
+            {
+                RotateBoy();
+            }
+
             if (Input.GetKey(KeyCode.LeftArrow))
-                _rigidbody2D.AddForce(Vector2.left * Config.PLAYER_MOVEMENT_SPEED);            
+                _rigidbody2D.AddForce(Vector2.left * Config.PLAYER_MOVEMENT_SPEED);
 
             if (Input.GetKey(KeyCode.RightArrow))
                 _rigidbody2D.AddForce(Vector2.right * Config.PLAYER_MOVEMENT_SPEED);
